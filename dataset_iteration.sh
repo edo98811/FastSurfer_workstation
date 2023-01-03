12dcf9ea7afd92896f275811e27454f7addcf8cd
edo98811 <edoardofilippi77@gmail.com>
2023-01-03 11:37:04 +0000

    aggiunto uno script bash per leggere tutte le immagini

--- dataset_iteration.sh
+++ dataset_iteration.sh
@@ -4,7 +4,8 @@
 for file in /media/neuropsycad/disk12t/EdoardoFilippiMasterThesis/implementations/unetsv3/dataset/*
 do
   echo "$file"
-  echo {cut -b -7 "$file"}
+  basename=${file##*/}
+  echo cut -b -7 "$basename"
 #   sudo docker run --gpus all -v /media/neuropsycad/disk12t/EdoardoFilippiMasterThesis/implementations/unetsv3/dataset:/data \
 #		              -v /media/neuropsycad/disk12t/EdoardoFilippiMasterThesis/implementations/fastsurfer_output:/output \
 #		              -v /media/neuropsycad/disk12t/HelenaPereira/freesurfer:/fs_license \
